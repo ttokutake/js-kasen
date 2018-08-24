8b8a4b39c6781b47a8ca8aa597780d7e35a8d43f
ttokutake <tadatoshi.tokutake@gmail.com>
2018-08-24 20:21:00 +0900

    Do "clean" and "build" before testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+
 module.exports = {
+  mode,
   output: {
     library: "Kasen",
     libraryTarget: "commonjs2",
