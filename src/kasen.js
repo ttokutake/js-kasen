7ad6a6b5182fb37d75372e0a6b7228ff56bb2034
ttokutake <tadatoshi.tokutake@gmail.com>
2018-09-16 22:58:09 +0900

    Implement Kasen as proxy

--- src/kasen.js
+++ src/kasen.js
@@ -1,3 +1,21 @@
 const { default: KasenArray } = require("./array");
+const { default: KasenObject } = require("./object");
 
-module.exports = KasenArray;
+function Kasen(coll) {
+  if (Array.isArray(coll)) {
+    return new KasenArray(coll);
+  }
+  if (typeof coll === "object") {
+    return new KasenObject(coll);
+  }
+  throw new TypeError('"coll" must be Array or Object');
+}
+
+Kasen.reduce = (coll, func, init) => {
+  if (Array.isArray(coll)) {
+    return KasenArray.reduce(coll, func, init);
+  }
+  throw new TypeError('"coll" must be Array');
+};
+
+module.exports = Kasen;
--- test/array.test.js
+++ test/array.test.js
@@ -3,7 +3,7 @@ import Kasen from "../dist/kasen";
 describe("Array", () => {
   describe("clone()", () => {
     test("ok", () => {
-      const array = new Kasen([1, 2, 3]).map(v => v + 1);
+      const array = Kasen([1, 2, 3]).map(v => v + 1);
       const array2 = array.clone().map(v => v + 1);
       expect(array.toJs()).toEqual([2, 3, 4]);
       expect(array2.toJs()).toEqual([3, 4, 5]);
@@ -19,7 +19,7 @@ describe("Array", () => {
         [[1, 2, 3], [2, 3, 4]]
       ];
       ios.forEach(([input, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.map(v => v + 1).toJs();
         expect(result).toEqual(expected);
       });
@@ -37,7 +37,7 @@ describe("Array", () => {
         [[1, 2, 3], [2]]
       ];
       ios.forEach(([input, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.filter(v => v % 2 === 0).toJs();
         expect(result).toEqual(expected);
       });
@@ -62,7 +62,7 @@ describe("Array", () => {
         [[1, 2, 3], 2, [1, 2]]
       ];
       ios.forEach(([input, num, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.take(num).toJs();
         expect(result).toEqual(expected);
       });
@@ -79,7 +79,7 @@ describe("Array", () => {
         [[1, 2, 3], [3, 2, 1]]
       ];
       ios.forEach(([input, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.reverse().toJs();
         expect(result).toEqual(expected);
       });
@@ -101,7 +101,7 @@ describe("Array", () => {
         [[1, 2], 2, [1, 2, 10]]
       ];
       ios.forEach(([input, index, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.set(index, 10).toJs();
         expect(result).toEqual(expected);
       });
@@ -118,7 +118,7 @@ describe("Array", () => {
         [[1, 2], 3]
       ];
       ios.forEach(([input, index]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const run = () => array.set(index, 10).toJs();
         expect(run).toThrow(RangeError);
       });
@@ -138,7 +138,7 @@ describe("Array", () => {
         [[1, 2, 3], 10, 16]
       ];
       ios.forEach(([input, init, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result =
           init === undefined
             ? array.reduce((acc, v) => acc + v)
@@ -168,7 +168,7 @@ describe("Array", () => {
         [[1, 3, 5], true]
       ];
       ios.forEach(([input, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.every(v => v % 2 === 1);
         expect(result).toBe(expected);
       });
@@ -188,7 +188,7 @@ describe("Array", () => {
         [[2, 4, 5], 5]
       ];
       ios.forEach(([input, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.find(v => v % 2 === 1);
         expect(result).toBe(expected);
       });
@@ -208,7 +208,7 @@ describe("Array", () => {
         [[1, 2, 4], 1]
       ];
       ios.forEach(([input, expected]) => {
-        const array = new Kasen(input);
+        const array = Kasen(input);
         const result = array.findLast(v => v % 2 === 1);
         expect(result).toBe(expected);
       });
