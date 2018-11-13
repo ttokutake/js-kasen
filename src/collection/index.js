73beba4ef99bfa6e4ec1ec28fedec0a2e97ac3eb
ttokutake <tadatoshi.tokutake@gmail.com>
2018-11-14 00:04:44 +0900

    Just add a TODO comment

--- src/collection/index.js
+++ src/collection/index.js
@@ -458,6 +458,7 @@ export default class Collection {
     throw new Error("not implemented");
   }
 
+  // TODO: func = v => v
   partition(fun) {
     if (!isFunction(fun)) {
       throw new TypeError("1st argument must be Function");
